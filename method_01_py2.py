74f0699396340c1d09adbf349a8a41974d9d8022
Raul <rchfe23@gmail.com>
2018-08-28 13:50:04 -0400

    add code for method1-Python 3

---
+++ method_01_py2.py
@@ -0,0 +1,20 @@
+import time
+import ipdb
+
+n_times = 10
+n_items = 10000
+my_dict = dict(zip(range(1, n_items+1), range(1, n_items+1)))
+#my_dict = {1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'd', 6: 'c'}
+durations = 0
+for i in xrange(n_times):
+    start_time = time.time()
+    #inv_dict = {v: k for k, v in my_dict.iteritems()}
+    inv_dict = {v: k for k, v in my_dict.items()}
+    duration = time.time() - start_time
+    durations += duration
+    print(duration)
+
+print("Avg: {} seconds".format(durations/n_times))
+#ipdb.set_trace()
+#print(my_dict)
+#print(inv_dict)
