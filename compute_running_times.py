1ff07ee028d72294661aa9cb266be2d5ea8fc4cf
Raul <rchfe23@gmail.com>
2018-08-29 00:14:08 -0400

    add args to method_01_py3

--- compute_running_times.py
+++ compute_running_times.py
@@ -1,8 +1,15 @@
 import argparse
+import os
+import shlex
+import subprocess
 import textwrap
+
+import ipdb
 
 
 VERSION = '0.1'
+python2_path = '/usr/local/bin/python2.7'
+python3_path = os.path.expanduser('~/miniconda3/envs/test_py3/bin/python')
 
 if __name__ == '__main__':
     description = '''
@@ -18,7 +25,7 @@ if __name__ == '__main__':
         Github project @ https://github.com/raul23/python_reverse_dict
         '''.format(VERSION)
     parser = argparse.ArgumentParser(
-        prog='compute_running_times',
+        # prog='compute_running_times',
         description=textwrap.dedent(description),
         formatter_class=argparse.RawTextHelpFormatter)
 
@@ -27,15 +34,48 @@ if __name__ == '__main__':
 
     help_arg = '''\
     Name of the method that reverses a dict:
-    method-01: makes use of dict comprehension, and the dict must contain
-               unique values
-    method-02: makes use of dict.get(), and the dict doesn't contain unique values
-    method-03: makes use of map(reversed,), and the type and order of the original
-               dict are preserved (if for example it is an OrderedDict)
+
+    method_01_py2: makes use of dict comprehension, and the dict must contain
+                   unique values
+    method_02_py2: makes use of dict.get(), and the dict doesn't contain unique values
+    method_03_py2: makes use of map(reversed,), and the type and order of the original
+                   dict are preserved (if for example it is an OrderedDict)
+
+    method_01_py3: Python 3 version of method-01-py2
+    method_02_py3: Python 3 version of method-02-py3
+    method_03_py3: Python 3 version of method-03-py2
     '''
-    parser.add_argument('-m', '--method-name', default='method-01', help=help_arg)
-    parser.add_argument('-n', '--number-times', default=10, type=int, help='')
-    parser.add_argument('-i', '--number-items', default=10000, type=int, help='')
-    parser.add_argument('-ui', '--use-items', action='store_true', help='')
-    parser.add_argument('-s', '--small-test', action='store_true', help='')
+    parser.add_argument('-m', '--method_name', default='method_01_py3', help=help_arg)
+    parser.add_argument('-n', '--number_times', default=10, type=int, help='')
+    parser.add_argument('-i', '--number_items', default=10000, type=int, help='')
+    parser.add_argument('-p', '--precision', default=4, type=int, help='')
+    parser.add_argument('-ui', '--use_items', action='store_true', help='')
+    parser.add_argument('-usd', '--use_setdefault', action='store_true', help='')
+    parser.add_argument('-s', '--small_test', action='store_true', help='')
     args = parser.parse_args()
+
+    options = ''
+    which_python = None
+    which_method = None
+    for k, v in args.__dict__.items():
+        if k == 'method_name':
+            pos = v.find('_py')
+            which_method = v[:pos]
+            if v[pos+1:] == 'py3':
+                which_python = python3_path
+            else:
+                which_python = python2_path
+        elif isinstance(v, bool):
+            options += "--{} ".format(k)
+        else:
+            options += "--{}={} ".format(k, v)
+    options = options.strip()
+    assert which_python is not None, "Can't determine which " \
+                                     "python version (2 or 3) to use for " \
+                                     "running the method's script"
+
+    cmd = '{} {}/{} {}'.format(which_python, which_method, args.method_name+'.py', options)
+    cmd = shlex.split(cmd)
+    result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout = result.stdout.decode('utf-8')
+    print(stdout)
--- method_01/method_01_py3.py
+++ method_01/method_01_py3.py
@@ -1,20 +1,41 @@
+import argparse
 import time
 import ipdb
 
-n_times = 10
-n_items = 10000
-my_dict = dict(zip(range(1,n_items+1), range(1, n_items+1)))
-# my_dict = {1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f'}
-inv_dict = {}
-durations = 0
-for i in range(n_times):
-    start_time = time.time()
-    inv_dict = {v: k for k, v in my_dict.items()}
-    duration = time.time() - start_time
-    durations += duration
-    print(duration)
 
-print("Avg: {} seconds".format(durations/n_times))
-ipdb.set_trace()
-print(my_dict)
-print(inv_dict)
+def method_01(**kwargs):
+    number_times = kwargs['number_times']
+    number_items = kwargs['number_items']
+    decimal_precision = kwargs['precision']
+    if kwargs['small_test']:
+        number_times = 1
+        number_items = 1
+        my_dict = {1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f'}
+    else:
+        my_dict = dict(zip(range(1, number_items + 1), range(-number_items, 0)))
+    inv_dict = {}
+    durations = 0
+    count = 1
+    for i in range(number_times):
+        start_time = time.time()
+        inv_dict = {v: k for k, v in my_dict.items()}
+        duration = time.time() - start_time
+        durations += duration
+        print('#{} Duration: {:.{}f}'.format(count, duration, decimal_precision))
+        count += 1
+
+    print('Avg: {:.{}f} seconds'.format((durations / number_items), decimal_precision))
+    if kwargs['small_test']:
+        print(my_dict)
+        print(inv_dict)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--number_times', default=10, type=int, help='')
+    parser.add_argument('-i', '--number_items', default=10000, type=int, help='')
+    parser.add_argument('-p', '--precision', default=4, type=int, help='')
+    parser.add_argument('-s', '--small_test', action='store_true', help='')
+    args, unknown = parser.parse_known_args()
+
+    method_01(**args.__dict__)
