1ae101472e927978f284ea55f6a0d335c0823353
Raul <rchfe23@gmail.com>
2018-08-31 16:11:40 -0400

    add method 02 python 3

--- scripts/method_02_py3.py
+++ scripts/method_02_py3.py
@@ -1,19 +1,19 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 import os
 import sys
 
-import ipdb
+# import ipdb
 
 # TODO: add this into env variable
 sys.path.insert(0, os.path.expanduser("~/PycharmProjects/github_projects/python_reverse_dict"))
 from reverse_dict.argparser_builder import ArgParserBuilder
-from reverse_dict.arguments import UseItemsArgument, get_common_arguments
+from reverse_dict.arguments import get_common_arguments
 from reverse_dict.config import cfg
-from reverse_dict.methods import Method02Py2
+from reverse_dict.methods import Method02Py3
 
 
-method_name = Method02Py2.__method_name__
-python_version = Method02Py2.__python_version__
+method_name = Method02Py3.__method_name__
+python_version = Method02Py3.__python_version__
 
 
 def run_method(args=None):
@@ -22,21 +22,20 @@ def run_method(args=None):
         script_description = '''
         %(prog)s v{}
 
-        %(prog)s works on Python 2 by reversing a dict's keys and values. It
+        %(prog)s works on Python 3 by reversing a dict's keys and values. It
         saves all the keys associated with the same values in a list. It makes
-        use of dict.get() and dict.iteritems() by default when populating the
-        reversed dict, and assumes that the dict contains NON-UNIQUE values.
+        use of dict.get() by default when populating the reversed dict, and
+        assumes that the dict contains NON-UNIQUE values.
 
         Github project @ {}
-        '''.format(cfg.version, Method02Py2.__method_name__, cfg.github_url)
-        list_arguments = [UseItemsArgument()]
-        list_arguments.extend(get_common_arguments())
+        '''.format(cfg.version, Method02Py3.__method_name__, cfg.github_url)
+        list_arguments = get_common_arguments()
         parser_builder = ArgParserBuilder(script_description=script_description,
                                           list_arguments=list_arguments)
         parser = parser_builder.get_parser()
         args, unknown = parser.parse_known_args()
         args = args.__dict__
-    method_02 = Method02Py2(**args)
+    method_02 = Method02Py3(**args)
     print('Args: {}'.format(args))
     if unknown is None:
         print('Unused args: {}'.format(method_02.get_unused_kwargs()))
