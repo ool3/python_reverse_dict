4b5a12f96d60078dc71bec1c0ed6c8f6cdbe74ac
Raul <rchfe23@gmail.com>
2018-09-04 01:19:31 -0400

    big fix: speed up reversing methods

--- reverse_dict/methods.py
+++ reverse_dict/methods.py
@@ -19,10 +19,6 @@ class Method (object):
         self.use_non_uniques = kwargs[UseNonUniquesArgument.__argument_name__]
         self.print_dicts = kwargs[PrintDictsArgument.__argument_name__]
         self.use_ordered_dict = kwargs[UseOrderedDictArgument.__argument_name__]
-        if self.use_ordered_dict:
-            self.dict_ = OrderedDict
-        else:
-            self.dict_ = dict
         try:
             # Python 2 uses xrange
             self.range_ = xrange
@@ -31,33 +27,33 @@ class Method (object):
             self.range_ = range
         self.run_times = 0
         self.orig_dict = None
-        self.inv_dict = None
-        self.init_orig_dict()
-        self.init_inv_dict()
+        self.inv_dict = self.get_empty_dict()
+        self._init_orig_dict()
 
-    def init_orig_dict(self):
+    def _init_orig_dict(self):
         if self.use_non_uniques:
             # First half
             k1 = ['k{}'.format(i) for i in self.range_(1, int(self.number_items / 2) + 1)]
             v1 = ['v{}'.format(i) for i in self.range_(1, int(self.number_items / 2) + 1)]
-            dict1 = self.dict_(zip(k1, v1))
+            dict1 = self.init_dict(zip(k1, v1))
             # Second half: different keys but same values as in first half
             k2 = ['k{}'.format(i) for i in self.range_(int(self.number_items / 2) + 1, self.number_items + 1)]
             v2 = ['v{}'.format(i) for i in self.range_(1, int(self.number_items / 2) + 2)]
-            dict2 = self.dict_(zip(k2, v2))
+            dict2 = self.init_dict(zip(k2, v2))
             # Merge both halves
             dict1.update(dict2)
             self.orig_dict = dict1
         else:
             k = ['k{}'.format(i) for i in self.range_(1, self.number_items + 1)]
             v = ['v{}'.format(i) for i in self.range_(1, self.number_items + 1)]
-            self.orig_dict = self.dict_(zip(k, v))
+            self.orig_dict = self.init_dict(zip(k, v))
 
-    def init_inv_dict(self):
-        self.inv_dict = self.dict_()
+    def init_dict(self, list_tuple):
+        dict_type = OrderedDict if self.use_ordered_dict else dict
+        return dict_type(list_tuple)
 
-    def compute_avg_run_time(self):
-        raise NotImplementedError()
+    def get_empty_dict(self):
+        return OrderedDict() if self.use_ordered_dict else {}
 
     @staticmethod
     def get_items(dict_):
@@ -69,19 +65,17 @@ class Method (object):
         unused_keys = keys_from_kwargs - all_keys
         return list(unused_keys)
 
-    def print_run_time(self, i, duration):
-        print('#{} Run time: {:.{}f}'.format(i, duration, self.precision))
-
     def print_avg_run_time(self):
         print('Avg run time: {:.{}f} seconds'.format(
             (self.run_times / self.number_times),
             self.precision))
         if self.print_dicts:
-            print("")
-            print('Original dict:\n{}'.format(self.orig_dict))
+            print('\nOriginal dict:\n{}'.format(self.orig_dict))
             print('Inverse dictionary:\n{}'.format(self.inv_dict))
 
-    @staticmethod
+    def print_run_time(self, i, duration):
+        print('#{} Run time: {:.{}f}'.format(i, duration, self.precision))
+
     def reset_data(func):
         @functools.wraps(func)
         def wrapper_reset_data(self, *args, **kwargs):
@@ -89,7 +83,13 @@ class Method (object):
             func(self, *args, **kwargs)
         return wrapper_reset_data
 
-    def reverse_dict(self, dict_):
+    @reset_data
+    def compute_avg_run_time(self):
+        for _ in self.range_(1, self.number_times + 1):
+            self.inv_dict = self.reverse_orig_dict()
+        self.print_avg_run_time()
+
+    def reverse_orig_dict(self):
         raise NotImplementedError()
 
     @staticmethod
@@ -98,8 +98,10 @@ class Method (object):
         def wrapper_timer(self, *args, **kwargs):
             wrapper_timer.num_calls += 1
             start_time = time.perf_counter()
+            # start_time = time.time()
             retval = func(self, *args, **kwargs)
             end_time = time.perf_counter()
+            # end_time = time.time()
             run_time = end_time - start_time
             self.run_times += run_time
             self.print_run_time(wrapper_timer.num_calls, run_time)
@@ -116,19 +118,20 @@ class MethodPy2(Method):
         from reverse_dict.arguments import UseItemsArgument
         # Extra options that are specific to Python2-based methods
         self.use_items = self.kwargs[UseItemsArgument.__argument_name__]
+        self.orig_dict_items = self.get_items(self.orig_dict)
 
-    def compute_avg_run_time(self):
-        raise NotImplementedError()
-
+    # Override parent's method
     def get_items(self, dict_):
-        if self.use_items:
-            return dict_.items()
-        else:
-            return dict_.iteritems()
+        # dict.iteritems() returns an iterator, not a list of values like
+        # dict.items() does
+        return dict_.items() if self.use_items else \
+            [(k, v) for k, v in dict_.iteritems()]
 
-    def reverse_dict(self, dict_):
+    # Override parent's method
+    def reverse_orig_dict(self):
         raise NotImplementedError()
 
+    # Override parent's method
     # No perf_counter() in Python2
     @staticmethod
     def timer(func):
@@ -159,21 +162,17 @@ class Method01Py2(MethodPy2):
     def __init__(self, **kwargs):
         super(Method01Py2, self).__init__(**kwargs)
 
-    @Method.reset_data
-    def compute_avg_run_time(self):
-        for _ in self.range_(1, self.number_times + 1):
-            self.inv_dict = self.reverse_dict(self.orig_dict)
-        self.print_avg_run_time()
-
     @MethodPy2.timer
-    def reverse_dict(self, orig_dict):
+    def reverse_orig_dict(self):
         # Dict comprehension template for Python 2.7+:
         # {key:value for (key,value) in dictionary.items()}
         # Dict comprehension template in Python2.6:
         # dict((key, value) for (key, value) in dictionary.items())
         # ref.: https://bit.ly/2Ce9xjm and https://stackoverflow.com/a/14507623
-        return self.dict_({v: k for k, v in self.get_items(orig_dict)})
-        # return self.dict_([(v, k) for k, v in self.get_items(orig_dict)])
+        if self.use_ordered_dict:
+            return OrderedDict((v, k) for k, v in self.orig_dict_items)
+        else:
+            return {v: k for k, v in self.orig_dict_items}
 
 
 class Method01Py3(Method):
@@ -186,17 +185,19 @@ class Method01Py3(Method):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-
-    @Method.reset_data
-    def compute_avg_run_time(self):
-        for _ in self.range_(1, self.number_times + 1):
-            self.inv_dict = self.reverse_dict(self.orig_dict)
-        self.print_avg_run_time()
+        self.orig_dict_items = self.get_items(self.orig_dict)
 
     @Method.timer
-    def reverse_dict(self, orig_dict):
-        return self.dict_({v: k for k, v in orig_dict.items()})
-        # return self.dict_([(v, k) for k, v in orig_dict.items()])
+    def reverse_orig_dict(self):
+        # TODO: Factorize. Method01Py2 and Method01Py3 have the same method.
+        import ipdb
+        ipdb.set_trace()
+        if self.use_ordered_dict:
+            return OrderedDict((v, k) for k, v in self.orig_dict_items)
+        else:
+            return {v: k for k, v in self.orig_dict_items}
+            # Initial insertion order not respected:
+            # return {(v, k) for k, v in self.orig_dict_items}
 
 
 class Method02Py2(MethodPy2):
@@ -212,17 +213,12 @@ class Method02Py2(MethodPy2):
     def __init__(self, **kwargs):
         super(Method02Py2, self).__init__(**kwargs)
 
-    @Method.reset_data
-    def compute_avg_run_time(self):
-        for _ in self.range_(1, self.number_times + 1):
-            # Init inverted dict
-            self.inv_dict = self.reverse_dict(self.orig_dict)
-        self.print_avg_run_time()
-
     @MethodPy2.timer
-    def reverse_dict(self, orig_dict):
-        inv_dict = self.dict_()
-        for k, v in self.get_items(orig_dict):
+    def reverse_orig_dict(self):
+        # TODO: Factorize. Method02Py2 and Method02Py3 have the same method.
+        # Init inverse dict
+        inv_dict = self.get_empty_dict()
+        for k, v in self.orig_dict_items:
             inv_dict[v] = inv_dict.get(v, [])
             inv_dict[v].append(k)
         return inv_dict
@@ -240,18 +236,13 @@ class Method02Py3(Method):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-
-    @Method.reset_data
-    def compute_avg_run_time(self):
-        for _ in self.range_(1, self.number_times + 1):
-            self.inv_dict = self.reverse_dict(self.orig_dict)
-        self.print_avg_run_time()
+        self.orig_dict_items = self.get_items(self.orig_dict)
 
     @Method.timer
-    def reverse_dict(self, orig_dict):
-        # Init inverted dict
-        inv_dict = self.dict_()
-        for k, v in orig_dict.items():
+    def reverse_orig_dict(self):
+        # Init inverse dict
+        inv_dict = self.get_empty_dict()
+        for k, v in self.orig_dict_items:
             inv_dict[v] = inv_dict.get(v, [])
             inv_dict[v].append(k)
         return inv_dict
@@ -269,16 +260,15 @@ class Method03Py2(MethodPy2):
     def __init__(self, **kwargs):
         super(Method03Py2, self).__init__(**kwargs)
 
-    @Method.reset_data
-    def compute_avg_run_time(self):
-        for _ in self.range_(1, self.number_times + 1):
-            self.inv_dict = self.reverse_dict(self.orig_dict)
-        self.print_avg_run_time()
-
     @MethodPy2.timer
-    def reverse_dict(self, orig_dict):
-        # NOTE: Equivalent --> orig_dict.__class__ AND self.dict_
-        return self.dict_(map(reversed, self.get_items(orig_dict)))
+    def reverse_orig_dict(self):
+        # TODO: Factorize. Method03Py2 and Method03Py3 have the same method
+        # NOTE: try self.orig_dict.__class__
+        if self.use_ordered_dict:
+            return OrderedDict(map(reversed, self.orig_dict_items))
+        else:
+            # return dict(map(reversed, self.orig_dict_items))
+            return dict(map(reversed, self.orig_dict_items))
 
 
 class Method03Py3(Method):
@@ -292,14 +282,13 @@ class Method03Py3(Method):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-
-    @Method.reset_data
-    def compute_avg_run_time(self):
-        for _ in self.range_(1, self.number_times + 1):
-            self.inv_dict = self.reverse_dict(self.orig_dict)
-        self.print_avg_run_time()
+        self.orig_dict_items = self.get_items(self.orig_dict)
 
     @Method.timer
-    def reverse_dict(self, orig_dict):
-        # NOTE: Equivalent --> orig_dict.__class__ AND self.dict_
-        return self.dict_(map(reversed, orig_dict.items()))
+    def reverse_orig_dict(self):
+        # NOTE: try self.orig_dict.__class__
+        if self.use_ordered_dict:
+            return OrderedDict(map(reversed, self.orig_dict_items))
+        else:
+            # return dict(map(reversed, self.orig_dict_items))
+            return dict(map(reversed, self.orig_dict_items))
